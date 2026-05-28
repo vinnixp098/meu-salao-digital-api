51dbf916858f94ee952373da9bddfa519ae2c574
Vinícius <vinicius.pires@apotiguar.com.br>
2026-05-28 18:24:58 -0300

    Subindo uptimer

---
+++ src/main/java/com/vinicius/meu_salao_digital_api/busines/controller/HealthController.java
@@ -0,0 +1,15 @@
+package com.vinicius.meu_salao_digital_api.busines.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/health")
+public class HealthController {
+
+    @GetMapping
+    public String health() {
+        return "OK";
+    }
+}
