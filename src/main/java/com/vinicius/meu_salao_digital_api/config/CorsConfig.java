af9acdda91970f788cb8c50046160bdd8b018cc2
Vinícius <vinicius.pires@apotiguar.com.br>
2026-07-04 16:27:48 -0300

    Subindo ajuste no CORS para permitir a porta 5173

--- src/main/java/com/vinicius/meu_salao_digital_api/config/CorsConfig.java
+++ src/main/java/com/vinicius/meu_salao_digital_api/config/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig {
       @Override
       public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-          .allowedOrigins("http://localhost:3000", "https://suculentus-ui.onrender.com")
+          .allowedOrigins("http://localhost:3000",  "http://localhost:5173")
           .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
           .allowedHeaders("*")
           .allowCredentials(true);
